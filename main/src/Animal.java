386d8c94a0a4a6f7335e9fda1533d93b013f7992
krzywyzenon <tomasz.wlazlo@yahoo.com>
2012-11-15 13:56:41 +0100

    Animal slice method added, plus constructor plus the beginning of main

--- main/src/Animal.java
+++ main/src/Animal.java
@@ -1,4 +1,33 @@
 
 public class Animal {
+	
+	String animalName;
+	
+	public Animal(String name){
+		animalName = name;
+	}
+	
+	public void slice(String tool){
+		switch(tool){
+		case "Katana":
+			System.out.println("With one clean arc slice you cut " + animalName +"'s body into two separate parts! Congratulations!");
+			break;
+		case "Long Sword":
+			System.out.println("With the mighty swing you cut " + animalName +"'s furry ugly head! Congratulations!");
+			break;
+		case "Knife":
+			System.out.println("With the firm thrust, you penetrate " + animalName +"'s body and reach its heart! Congratulations!");
+			break;
+		case "Scissors":
+			System.out.println("Really???? You want to slice animal with scissors???? Are you simply dumb or do you have a medical state??");
+			break;
+		default:
+			System.out.println("How can you slice" + animalName + " if you haven't chosen a tool??? DUH!!!!" );
+			break;
+		}
+				
+			
+			
+	}
 
 }
--- main/src/main.java
+++ main/src/main.java
@@ -1,11 +1,53 @@
+import java.util.Scanner;
+
 
 public class main {
-
+ 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		String weapon = null;
+		System.out.println("Name your animal:");
+		String name = input.next();
+		Animal victim = new Animal(name);
+		System.out.println("What do you want to do with" + name +"?");
+		System.out.println("1. Slice");
+		int torture = input.nextInt();
+		switch (torture){
+		case 1:
+			System.out.println("Choose your tool");
+			System.out.println("1. Katana");
+			System.out.println("2. Longsword");
+			System.out.println("3. Knife");
+			System.out.println("4. Scissors");
+			
+			int tool = input.nextInt();
+		switch(tool){
+		case 1:
+			weapon = "Katana";
+			break;
+		case 2:
+			weapon = "Long Sword";
+			break;
+		case 3:
+			weapon = "Knife";
+			break;
+		case 4:
+			weapon = "Scissors";
+			break;
+			default:
+			break;
+		}
+			victim.slice(weapon);
+			break;
+			
+		default:
+			break;
+			
+			
+		}
 
 	}
 
