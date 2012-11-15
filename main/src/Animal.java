bd06f54a087ca63ca9bfe728b667e15a3353fa3f
unknown <Cetin@.(none)>
2012-11-15 14:26:37 +0100

    Added poke method in Animal.

--- main/src/Animal.java
+++ main/src/Animal.java
@@ -24,10 +24,17 @@ public class Animal {
 		default:
 			System.out.println("How can you slice" + animalName + " if you haven't chosen a tool??? DUH!!!!" );
 			break;
-		}
-				
-			
-			
+		}		
 	}
-
+	
+	public void poke(int hardness){
+		if(hardness >= 0
+		&& hardness < 10){
+			System.out.println("What? Huh?");
+		}
+		
+		else if(hardness > 10){
+			System.out.println("Grrr");
+		}
+	}
 }
