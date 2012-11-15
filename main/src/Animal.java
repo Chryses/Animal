5ea5e01ea545321923a253fca2885e9cdc52a603
unknown <Cetin@.(none)>
2012-11-15 14:14:53 +0100

    Adding poke.

--- main/src/Animal.java
+++ main/src/Animal.java
@@ -1,4 +1,14 @@
 
 public class Animal {
-
+	
+	public void poke(int hardness){
+		if(hardness >= 0
+		&& hardness < 10){
+			System.out.println("What? Huh?");
+		}
+		
+		else if(hardness > 10){
+			System.out.println("Grrr");
+		}
+	}
 }
